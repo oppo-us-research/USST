92e19145855bb865a57773285970cca15eff171d
Cogito2012 <wtbao2018@gmail.com>
2023-07-25 19:10:38 -0400

    clean code

--- src/models/transformer_layers.py
+++ src/models/transformer_layers.py
@@ -75,8 +75,8 @@ class ScaledDotProductAttention(nn.Module):
     def forward(self, q, k, v, mask=None):
         attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
         if mask is not None:
-            attn = attn.masked_fill(mask == 0, -1e9)  # Fills elements of attn with -1e9 where mask==0. 
-        attn = self.dropout(F.softmax(attn, dim=-1))  # nn.Dropout(p=0.1)
+            attn = attn.masked_fill(mask == 0, -1e9)
+        attn = self.dropout(F.softmax(attn, dim=-1))
         output = torch.matmul(attn, v)
 
         return output, attn
@@ -109,10 +109,10 @@ class MultiHeadAttention(nn.Module):
             k = k.reshape(B, Nk, self.num_heads, C // self.num_heads).transpose(1, 2)
             v = v.reshape(B, Nv, self.num_heads, C // self.num_heads).transpose(1, 2)
         if mask is not None:
-            mask = mask.unsqueeze(1)  # (B, 1, T, T)
+            mask = mask.unsqueeze(1)
 
         x, attn = self.attention(q, k, v, mask=mask)
-        x = x.transpose(1, 2).reshape(B, Nq, C)  # （B, T, D）
+        x = x.transpose(1, 2).reshape(B, Nq, C)
         if self.with_qkv:
            x = self.proj(x)  # （B, T, D)
            x = self.proj_drop(x)  # nn.Dropout(p=0)
@@ -143,47 +143,13 @@ class EncoderBlock(nn.Module):
 
 
     def forward(self, x, mask=None):
-        """ x: (B, T, D)
-            mask: # (B, T)
-        """
         src_mask = None
         if mask is not None:
             src_mask = get_pad_mask(mask, pad_idx=0)  # (B, 1, T)
-        # transformer block
         x_norm = self.norm1(x)
         x = x + self.drop_path(self.attn(q=x_norm, k=x_norm, v=x_norm, mask=src_mask))
         x = x + self.drop_path(self.mlp(self.norm2(x)))
         return x
-    
-
-
-class DecoderBlock(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0.1, act_layer=nn.GELU, norm_layer=nn.LayerNorm):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.self_attn = MultiHeadAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias,
-                                            qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-
-        self.norm2 = norm_layer(dim)
-        self.enc_dec_attn = MultiHeadAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias,
-                                               qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop)
-
-        self.norm3 = nn.LayerNorm(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim, act_layer=act_layer, drop=drop)
-
-    def forward(self, tgt, memory, memory_mask=None, trg_mask=None):  # memory (B, 5, 512)
-        tgt_2 = self.norm1(tgt)  # (B, 1, 512)
-        tgt = tgt + self.drop_path(self.self_attn(q=tgt_2, k=tgt_2, v=tgt_2, mask=trg_mask))  # (B, 1, 512)
-        # attention between a single future frame and 5 elems in memory
-        # q: (B, 1, 512),  k: (B, 5, 512),  v: (B, 5, 512)
-        tgt = tgt + self.drop_path(self.enc_dec_attn(q=self.norm2(tgt), k=memory, v=memory, mask=memory_mask))
-        tgt = tgt + self.drop_path(self.mlp(self.norm2(tgt)))
-        return tgt
-
 
 
 class Encoder(nn.Module):
@@ -221,58 +187,3 @@ class Encoder(nn.Module):
         
         x = self.norm(x)
         return x
-
-
-
-class Decoder_PositionalEmbedding(nn.Module):
-    def __init__(self, d_model, seq_len):
-        super(Decoder_PositionalEmbedding, self).__init__()
-        self.position_embedding = nn.Parameter(torch.zeros(1, seq_len, d_model))
-
-    def forward(self, x):
-        x = x + self.position_embedding[:, :x.shape[1], :]
-        return x
-
-
-class Decoder(nn.Module):
-    def __init__(self, in_features, embed_dim=512, depth=6, num_heads=8, mlp_ratio=4.,
-                 qkv_bias=False, qk_scale=None, drop_rate=0., attn_drop_rate=0.,
-                 drop_path_rate=0.1, norm_layer=nn.LayerNorm, dropout=0.,
-                 time_embed_type=None, num_frames=None):
-        super().__init__()
-        self.depth = depth
-        self.dropout = nn.Dropout(dropout)
-        self.num_features = self.embed_dim = embed_dim
-
-        self.trg_embedding = nn.Linear(in_features, embed_dim)
-
-        if time_embed_type is None or num_frames is None:
-            time_embed_type = 'sin'
-        self.time_embed_type = time_embed_type
-        if not self.time_embed_type == "sin" and num_frames is not None:
-            self.time_embed = Decoder_PositionalEmbedding(embed_dim, seq_len=num_frames)
-        else:
-            self.time_embed = PositionalEncoding(embed_dim)
-        self.time_drop = nn.Dropout(p=drop_rate)
-
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, self.depth)]
-        self.decoder_blocks = nn.ModuleList([DecoderBlock(
-            dim=embed_dim, num_heads=num_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale,
-            drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[i], norm_layer=norm_layer)
-            for i in range(self.depth)])
-        self.norm = norm_layer(embed_dim)
-
-    @torch.jit.ignore
-    def no_weight_decay(self):
-        return {'pos_embed', 'time_embed'}
-
-    def forward(self, trg, memory, memory_mask=None, trg_mask=None):
-        trg = self.trg_embedding(trg)
-        trg = self.time_embed(trg)
-        trg = self.time_drop(trg)  # (B, 1, D)
-
-        for blk in self.decoder_blocks:
-            trg = blk(trg, memory, memory_mask=memory_mask, trg_mask=trg_mask)
-
-        trg = self.norm(trg)
-        return trg
